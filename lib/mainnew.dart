183c0c600cbadb35461c665f6b2371177e06b9cd
Huzaifa <huzaifatawab33@gmail.com>
2023-11-13 14:49:28 +0500

    update

---
+++ lib/mainnew.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Notification App',
+      home: NotificationPage(),
+    );
+  }
+}
+
+class NotificationPage extends StatefulWidget {
+  const NotificationPage({super.key});
+
+  @override
+  _NotificationPageState createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends State<NotificationPage> {
+  final DatabaseReference _databaseReference =
+      FirebaseDatabase.instance.ref('BB-0456/Gps/Engine_Status');
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    initializeNotifications();
+    listenToChanges();
+  }
+
+  Future<void> initializeNotifications() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('mipmap/ic_launcher');
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
+  }
+
+  void listenToChanges() {
+    _databaseReference.onValue.listen((DatabaseEvent event) {
+      // Assuming your database structure is like { "value": true }
+      print(event.snapshot.value as bool);
+      bool value = event.snapshot.value as bool;
+
+      if (value) {
+        showOnNotification();
+      } else {
+        showOffNotification();
+      }
+    });
+  }
+
+  Future<void> showOffNotification() async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      'your_channel_description',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Car Engine Shutdown',
+      'testing',
+      platformChannelSpecifics,
+      payload: 'item x',
+    );
+  }
+
+  Future<void> showOnNotification() async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      'your_channel_description',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      1,
+      'Car Engine Started',
+      'Notification Body',
+      platformChannelSpecifics,
+      payload: 'item x',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notification App'),
+      ),
+      body: const Center(
+        child: Text('Listen to changes in real-time database'),
+      ),
+    );
+  }
+}
