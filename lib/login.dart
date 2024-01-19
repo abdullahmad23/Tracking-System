1b987149e4ffd899332450aa06ab251e7d5d2b4e
Ahmed <akhan5544266@gmail.com>
2024-01-19 15:15:13 +0500

    #

--- lib/login.dart
+++ lib/login.dart
@@ -22,8 +22,8 @@ class _LoginPageState extends State<LoginPage> {
   void initState() {
     User? auth=  FirebaseAuth.instance.currentUser;
     if (auth!=null) {
-      Navigator.of(context).push(MaterialPageRoute(builder: ((context) => UserHome()
-      )));
+    
+    route();
     }
     super.initState();
   }
